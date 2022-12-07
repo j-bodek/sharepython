0a39fe48ca57c227ba27dbea910f4f8ff7d3b173
Liljack118 <kamilzajaczek4@gmail.com>
2022-12-07 21:43:46 +0100

    add clone.py to clone microservices repositories automatically

---
+++ clone.py
@@ -0,0 +1,37 @@
+import subprocess
+import os
+import sys
+
+
+def main():
+    """
+    This function is used to cloning microservices
+    repositories automatically
+    """
+
+    class colors:
+        YELLOW = "\033[33m"
+        GREEN = "\033[32m"
+        ENDC = "\033[m"
+
+    services = [
+        ("https://github.com/LilJack118/sharepython-frontend.git", "frontend"),
+        ("https://github.com/LilJack118/sharepython-api.git", "api"),
+    ]
+
+    def git(*args):
+        return subprocess.check_call(["git"] + list(args))
+
+    for repo, name in services:
+        if not os.path.isdir(name):
+            sys.stdout.write(
+                colors.GREEN + f"Cloning {name} repository\n" + colors.ENDC
+            )
+            # clone repository and rename it
+            git("clone", repo, name)
+        else:
+            sys.stdout.write(colors.YELLOW + f"{name} folder exist\n" + colors.ENDC)
+
+
+if __name__ == "__main__":
+    main()
